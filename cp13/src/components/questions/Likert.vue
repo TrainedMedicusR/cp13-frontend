8ca54c6efcb1adbda7868a00bc2d00fcb3cc12a4
Wenbo Li <weli6728@uni.sydney.edu.au>
2021-09-23 01:51:03 +0800

    Feature: SurveyMain fixed view

--- cp13/src/components/questions/Likert.vue
+++ cp13/src/components/questions/Likert.vue
@@ -1,61 +1,191 @@
 <template>
-<div class="TableList">
-        <div class="list">
-            <table class="listTab" cellspacing="0" cellpadding="0">
-                <thead>
-                    <tr>
-                        <th v-for="(item) in tableHead" :key="item.key"> {{item.title}}</th>
-                        <th> {{selector_title}} </th>
-                    </tr>
+        <div class = "LikertMatrix">
+    <li id="form-field-3" class="likert">
+        <table cellspacing="0">
+            <caption id= "title3">
+                Rank from 1 to 10.
+            </caption>
+            <thead>
 
-                </thead>
-                <tbody>
-                    <tr v-for="(item) in tableData" :key="item.openId">
-                        <td v-for="(val,index) in tableHead" :key="index">  
-                                             
-                            <div v-if="item[val.key]=='radio1'">
-                                <input id="Field1_1" name="Field1" type="radio" value= val.key />
-                            </div>
-                            <div v-else-if="item[val.key]=='radio2'">
-                                <input id="Field2_1" name="Field2" type="radio" value= val.key />
-                            </div>
-                            <div v-else-if="item[val.key]=='radio3'">
-                                <input id="Field3_1" name="Field3" type="radio" value= val.key />
-                            </div>
-                            <div v-else-if="item[val.key]=='radio4'">
-                                <input id="Field4_1" name="Field4" type="radio" value= val.key />
-                            </div>
-                            <div v-else-if="item[val.key]=='radio5'">
-                                <input id="Field5_1" name="Field5" type="radio" value= val.key />
-                            </div>
+                <th>&nbsp;</th>
+                <td>{{column1}}</td>
+                <td>{{column2}}</td>
+                <td>{{column3}}</td>
+                <td>{{column4}}</td>
+                <td>{{column5}}</td>
 
-                            <div v-else>
-                              <div v-if= "val.key== 'category'">
-                                <div v-if="item[val.key]=='Others'">
-                                  {{item[val.key]}}<input id="Field6_1" name="Field6" type="url" class="field text medium" value="" maxlength="25" tabindex="36" /> 
-                                </div>
-                                <div v-else> {{item[val.key]}} </div>
-                              </div>
-                            </div>
-                        </td>
-                        
-                        <td class = "selector">
-                        <div>
-                            <select>
-                            <option v-for="option in options" v-bind:key="option.value">
-                              {{ option.value }}
-                            </option>
-                            </select>
-                          </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table> 
+            </thead>
+            <tbody>
+
+            <tr class="statement1">
+            <th>
+                <label>
+                    {{row1}}
+                </label>
+            </th>
+
+                <li v-for="item in items" :key="item.message">
+                    <td title= item.value>
+                    <input id="Field1_1" name="Field1" type="radio" value= item.value />
+                    </td>
+                </li>
+
+                <td title="Extrme">
+                <input id="Field1_1" name="Field1" type="radio" value="Extreme" />
+                </td>
+                <td title="Very">
+                <input id="Field1_2" name="Field1" type="radio" value="Very" />
+                </td>
+                <td title="Slightly">
+                <input id="Field1_3" name="Field1" type="radio" value="Slightly" />
+                </td>
+                <td title="Not At All">
+                <input id="Field1_4" name="Field1" type="radio" value="Not At All" />
+                <td>
+                <div>
+                    <select v-model="selected1">
+                    <option v-for="option in options" v-bind:key="option.value">
+                      {{ option.text }}
+                    </option>
+                    </select>
+                    </div>
+                    <span>Selected: {{ selected1 }}</span>
+                </td>
+            </tr>
+
+
+            <tr class="statement2">
+
+                <th>
+                <label for="Field2">{{row2}}</label>
+                </th>
+                <td title="Extrme">
+                <input id="Field2_1" name="Field2" type="radio" value="Extreme" />
+                </td>
+                <td title="Very">
+                <input id="Field2_2" name="Field2" type="radio" value="Very" />
+                </td>
+                <td title="Slightly">
+                <input id="Field2_3" name="Field2" type="radio" value="Slightly" />
+                </td>
+                <td title="Not At All">
+                <input id="Field2_4" name="Field2" type="radio" value="not" />
+                </td>
+                <td>
+                <div>
+                    <select v-model="selected2">
+                    <option v-for="option in options" v-bind:key="option.value">
+                      {{ option.text }}
+                    </option>
+                    </select>
+                    </div>
+                    <span>Selected: {{ selected2 }}</span>
+                </td>
+                </tr>
+
+
+                <tr class="statement3">
+                <th><label for="Field3">{{row3}}</label></th>
+
+                <td title="Extreme">
+                <input id="Field3_1" name="Field3" type="radio" value="Extreme" />
+
+
+                </td>
+                <td title="Very">
+                <input id="Field3_2" name="Field3" type="radio" value="Very" />
+
+                </td>
+                <td title="Slightly">
+                <input id="Field3_3" name="Field3" type="radio" value="Slightly" />
+
+                </td>
+                <td title="Not At All">
+                <input id="Field3_4" name="Field3" type="radio" value="Not At All!" />
+                </td>
+
+                <td>
+                <div>
+
+                    <select v-model="selected3">
+                    <option v-for="option in options" v-bind:key="option.value">
+                      {{ option.text }}
+                    </option>
+                    </select>
+                    </div>
+                    <span>Selected: {{ selected3 }}</span>
+                </td>
+            </tr>
+
+
+            <tr class="statement4">
+                <th>
+                <label for="Field4">{{row4}}</label>
+                </th>
+                <td title="Extreme">
+                <input id="Field4_1" name="Field6" type="radio" value="Extreme" />
+                </td>
+                <td title="Very">
+                <input id="Field4_2" name="Field6" type="radio" value="Very" />
+                </td>
+                <td title="Slightly">
+                <input id="Field4_3" name="Field6" type="radio" value="Slightly" />
+                </td>
+                <td title="Not At All">
+                <input id="Field4_4" name="Field6" type="radio" value="Not At All" />
+                </td>
+
+                <td>
+                <div>
+                    <div>
+                    <select v-model="selected4">
+                    <option v-for="option in options" v-bind:key="option.value">
+                      {{ option.text }}
+                    </option>
+                    </select>
+                    </div>
+                    <span>Selected: {{ selected4 }}</span>
+                </div>
+                </td>
+            </tr>
+
+            <tr class="statement5">
+                <th>
+                <label for="Field5">{{textfield}}</label>
+                <div>
+                    <input id="Field" name="Field" type="url" class="field text medium" value="" maxlength="25" tabindex="36" />
+                </div>
+                </th>
+                <td title= "Extreme">
+                <input id="Field5_1" name="Field8" type="radio" value= title />
+                </td>
+                <td title="Very">
+                <input id="Field5_2" name="Field8" type="radio" value= title />
+                </td>
+                <td title="Slightly">
+                <input id="Field5_3" name="Field8" type="radio" value= title/>
+                </td>
+                <td title="Not At All">
+                <input id="Field5_4" name="Field8" type="radio" value= title />
+                </td>
+
+                <td>
+                <div>
+                    <select v-model="selected5">
+                    <option v-for="option in options" v-bind:key="option.value">
+                      {{ option.text }}
+                    </option>
+                    </select>
+                    </div>
+                    <span>Selected: {{ selected5 }}</span>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+        </li>
         </div>
-</div>
-</template>
- 
 
+</template>
 
 
 <script>
@@ -69,29 +199,34 @@ export default {
       quantityListing: null,
       bestListing: null,
       host:location.hostname,
-      
-      tableHead:[
-                {key:"category",title:"Category"},
-                {key:"extreme",title:"Extreme Important"},
-                {key:"very",title:"Very Important"},
-                {key:"slight",title:"Slightly Important"},
-                {key:"not",title:"Not Important At All"},
-            ],  
-      tableData:[
-          {category:"Your prior knowledge",extreme:"radio1",very:"radio1",slight:"radio1",not:"radio1"}, 
-          {category:"News source",extreme:"radio2",very:"radio2",slight:"radio2",not:"radio2"}, 
-          {category:"Headline claim",extreme:"radio3",very:"radio3",slight:"radio3",not:"radio3"}, 
-          {category:"Image associated with post",extreme:"radio4",very:"radio4",slight:"radio4",not:"radio4"}, 
-          {category:"Others",extreme:"radio5",slight:"radio5",very:"radio5",not:"radio5"}
-          
+
+      selected1: "Neither increase nor decrease",
+      selected2: "Neither increase nor decrease",
+      selected3: "Neither increase nor decrease",
+      selected4: "Neither increase nor decrease",
+      selected5: "Neither increase nor decrease",
+
+      column1:"Extreme Important",
+      column2:"Very Important",
+      column3:"Slightly Important",
+      column4:"Not Important",
+      column5:"Increase/decrease credibility",
+      columns:[{name:"Extreme"},{name: "Very"}, {name:"Slightly"}, {name:"Not at all"}, {name:"increase or decrease"}],
+      //rows:[{name:"Your prior knowledge"},{name: "News source"}, {name:"Headline claim"}, {name:"Image associated with post"}],
+      textfield:"others",
+      option1:"Neither increase nor decrease",
+      option2:"Increase",
+      option3:"Decrease",
+      row1:"Your prior knowledge",
+      row2:"News source",
+      row3: "Headline claim",
+      row4:"Image associated with post",
+      options: [
+        { text: 'Neither increase or decrease', value: 'Neither' },
+        { text: 'Increase', value: 'Increase' },
+        { text: 'Decrease', value: 'Decrease' }
       ],
-      options:[{value:" "},{value:"Neither increase nor decrease"}, {value:"Increase"}, {value:"Decrease"}],
-      selector_title: "Increase/Decrease Credibility"
-      
     }
-  },
-  mounted(){
-    this.initPage();
   },
   methods: {
     initPage() {
@@ -117,71 +252,196 @@ export default {
 </script>
 
 <style scoped>
- 
-.list{
-  width: 98%;
-  margin-left: 1%;
-  overflow-x:auto;
-}
-table.listTab{
-    width: 50%;
-    font-size: 14px;
-    color: #6a6a6a;
-    border: 1px solid #ececed;
-    padding: 1px;
-    background-color: #fff;
-}
-table.listTab th{
-    background-color: #ecf0f4;
-    padding: 14px 8px;
-    text-align: left;
-    font-size:14px;
-    border: 1px solid #ecf0f4;
-    min-width: 40px;
-    color:#292d31;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-}
-table.listTab th.check{
-    min-width: 20px;
+body {
+  background-color: #EFF2F5;
 }
 
-
-table.listTab tbody > tr td{
-    padding: 16px 8px;
-    text-align: center;
-    font-size: 14px;
-    border-bottom: 1px solid #cacacf;
-    vertical-align: middle;
-    word-break: break-all;
-    min-width: 80px;
-    overflow: hidden;
-    white-space: nowrap;
-    text-overflow: ellipsis;
+.surveyTitle {
+  text-align: center;
 }
 
-table.listTab tbody > tr td:first-child{
-    width: 50px;
-}
-table.listTab tbody > tr:hover{
-    background-color: #d1e1f1;
-}
-table.listTab tbody > tr.check{
-    background-color: #dfeefc;
-}
-table.listTab tbody > tr:last-child td{
-    border-bottom: 0px;
-}
-table.listTab tbody > tr td a{
-    color: #248bfc;
-}
-table.listTab tbody > tr td input{
-border:1px solid #248bfc;
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 800px;
+  margin: 0 auto;
+  padding: 20px;
 }
 
-table.listTab tbody > tr td div select{
+.wrapper{
+  width: 1280px;
+  max-width: 100%;
+  overflow: hidden;
+  margin: 0 auto;
+  padding: 40px;
+}
+
+.ele {
+  width: 40%;
+  height: 400px;
+}
+
+.ele:nth-child(3n+1) {
   width: 100%;
 }
+
+
+
+.question{
+  width: 60%;
+  background-color: white;
+  text-align: center;
+}
+
+
+.picWall{
+  width: 80%;
+  height: 230px;
+  border: 4px solid;
+  /*border-image: linear-gradient(45deg, turquoise, greenyellow) 1;*/
+  margin: 20px auto;
+  overflow: hidden;
+  text-align: left;
+}
+
+.LikertMatrix{
+    transform:scale(0.8, 0.8);
+}
+
+body {
+  font-family: "Lucida Grande","Lucida Sans Unicode", Tahoma, sans-serif;
+  line-height: 1.4;
+  padding: 30px;
+}
+
+.form-header {
+  margin: 0 0 1em 0;
+  padding: 0 0 1em 0;
+  border-bottom: 1px dotted #ccc
+}
+
+.text {
+  font-size: 120%;
+}
+
+form ul {
+  padding: 0;
+}
+form li {
+  list-style: none;
+  padding: 0.5em;
+  clear: both;
+  overflow: hidden;
+  position: relative;
+}
+form li span {
+  margin: 0 .3em 0 0;
+  float: left;
+}
+form li div span {
+  margin: 0;
+  display: block;
+  width: 50%;
+  float: left;
+}
+label span,
+.section span,
+p span,
+.likert span {
+  display: inline;
+  float: none;
+}
+form li div label,
+form li span label {
+  font-size: 50%;
+  padding-top: 0.25em;
+  clear: both;
+  display: block;
+}
+fieldset {
+  display: block;
+  border: none;
+  margin: 0;
+  padding: 0;
+}
+label.desc,
+legend {
+  font-weight: bold;
+  color: #222;
+  padding: 0 0 0.15em 0;
+  margin: 0 0 0.15em 0;
+  display: block;
+}
+label.choice {
+  display: block;
+  cursor: pointer;
+  font-size: 50%;
+  line-height: 50%;
+  margin: -1.25em 0 0 1.5em;
+  padding: 0 0 0.25em 0;
+}
+.likert table {
+  margin: 0 0 1em 0;
+  width: 60%;
+  border: 1px solid #dedede;
+}
+.likert caption {
+  font-weight: bold;
+  text-align: left;
+  margin: 0 0 0.5em 0;
+}
+.likert input {
+  margin: 0.25em 0;
+}
+.likert tbody td label {
+  display: block;
+}
+.likert thead td,
+.likert thead th {
+  background-color: #e6e6e6
+}
+.likert td {
+  border-left: 1px solid rgb(228, 222, 222);
+  border-right: 1px solid rgb(238, 231, 231);
+  text-align: center;
+  padding: 0.25em;
+  width: 100px;
+}
+.likert thead td {
+  font-size: 85%;
+  padding: 5px 6px;
+}
+.likert th,
+.likert td {
+  border-bottom: 1px solid #dedede;
+}
+.likert tbody th {
+  padding: 8px 8px;
+  text-align: left;
+}
+.likert tbody th label {
+  color: #222;
+  font-weight: bold
+}
+.likert tbody tr.alt td,
+.likert tbody tr.alt th {
+  background-color: #f5f5f5;
+}
+.likert tbody tr:hover td,
+.likert tbody tr:hover th {
+  background-color:#FFFFCF;
+}
+form li.section {
+  padding: 1em 0 0 0;
+}
+form li.section h3 {
+  font-size: 11%;
+  line-height: 13%;
+  margin: 0 0 3px 0;
+  padding: 12px 1% 0 1%;
+  border-top: 1px dotted #ccc
+}
+
 
 </style>
