1ef67ce93d60a600c28c16af19abeb113f4b202f
Wenbo Li <weli6728@uni.sydney.edu.au>
2021-09-27 02:16:46 +0800

    Feature: response

--- cp13/src/components/SwitchButton.vue
+++ cp13/src/components/SwitchButton.vue
@@ -11,7 +11,8 @@
 </template>
 
 <script>
-import {tempStorage} from "../utils/storage";
+import {tempStorage,storage} from "../utils/storage";
+import {postResp} from "../api/postSurvey";
 
 export default {
   name: "SwitchButton",
@@ -23,16 +24,29 @@ export default {
       let current = tempStorage.get(this.$route.params.id+"CURRENT");
       let total  = tempStorage.get(this.$route.params.id+"TOTAL");
       if (current < total){
-        current += 1;
+
         // window.scrollTo({
         //   left: 0,
         //   top: 0,
         //   behavior: 'smooth'
         // })
-        tempStorage.set(this.$route.params.id+"CURRENT",current);
-        location.reload();
+        this.submit("",this.response).then(response=>{
+          if (response.status === 200){
+            current += 1;
+            tempStorage.set(this.$route.params.id+"CURRENT",current);
+            location.reload();
+          } else {
+            alert("Network Error")
+          }
+        })
       } else {
-        alert("Thank you for your participation!");
+        this.submit("",this.response).then(response=>{
+          if (response.status === 200){
+            alert("Thank you for your participation!");
+          } else {
+            alert("Network Error")
+          }
+        })
         sessionStorage.clear();
       }
 
@@ -53,8 +67,11 @@ export default {
         alert("This is the first question")
       }
     },
-    submit(surveyID, questionID, content, identifier) {
-      //TODO
+    submit(contactINFO, content) {
+      let questionID = tempStorage.get(this.$route.params.id+"CURRENT");
+      let surveyID = tempStorage.get(this.$route.params.id+"sid");
+      let identifier = storage.get(this.$route.params.id);
+      return postResp(surveyID, questionID, contactINFO, content, identifier);
     }
   }
 }
