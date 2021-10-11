81a1578fc5ac185238e34baf77b21f9955996c54
tqiu2998 <tqiu2998@uni.sydney.edu.au>
2021-10-11 19:23:17 +1100

    fix the initial value of rtl indicator

--- cp13/src/components/DragAndDrop.vue
+++ cp13/src/components/DragAndDrop.vue
@@ -47,7 +47,7 @@ export default {
   components: {SwitchButton, NewsFrame},
   data () {
     return {
-      rtl:true,
+      rtl:'',
       response: "",
       responseJSON:{},
       isExpanded:true,
