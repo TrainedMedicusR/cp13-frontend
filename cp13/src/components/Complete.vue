29f10e2f448c6618747d533d448d9f4931c6aceb
tqiu2998 <tqiu2998@uni.sydney.edu.au>
2021-10-11 12:26:56 +1100

    feature: drag and drop rtl adaptation; 404 and thank you page

--- cp13/src/components/Complete.vue
+++ cp13/src/components/Complete.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    <h1>Thank you for your participation!</h1>
+  <div class = container>
+    <div class="picture">
+      <img src="../assets/usyd.jpeg" alt=""/>
+    </div>
+    <div>
+      <h1>Thank you for your participation!</h1>
+    </div>
   </div>
 </template>
 
@@ -11,6 +16,23 @@ export default {
 </script>
 
 <style scoped>
+.picture{
+  border: 2px solid grey;
+  width: 600px;
+  /*margin: 0 auto;*/
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.picture img{
+  /*height: 250px;*/
+  width: 100%;
+  height:100%;
+  max-width: 100%;
+  max-height: 100%;
+  margin: auto;
+
+}
 h1{
   width: 560px;
   margin: 0 auto;
--- cp13/src/components/NotFound.vue
+++ cp13/src/components/NotFound.vue
@@ -1,18 +1,59 @@
 <template>
-  <h1>404 NOT FOUND</h1>
+  <div class="error-block">
+    <div class="grids-2">
+      <div class="right-grid">
+        <h2>404</h2>
+        <h3>Not Found</h3>
+        <hr>
+        <h3>Cannot find specified survey</h3>
+        <h3>Please check if you entered the correct route!</h3>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "NotFound"
+  name: "NotAuthorised"
 }
 </script>
 
 <style scoped>
-h1{
-  width: 300px;
-  margin: 0 auto;
+h1,
+h2,
+h3{
+  margin: 0;
+  padding: 0
 }
 
+.error-block {
+  text-align: center;
+  align-items: center;
+}
 
+.error-block h2 {
+  color: #fff;
+  font-size: 16em;
+  margin: 20px;
+  line-height: .8em;
+  font-weight: bold;
+  text-transform: uppercase;
+  -webkit-text-fill-color: transparent;
+  background: -webkit-linear-gradient(transparent, transparent);
+  background: url(../assets/usyd.jpeg) repeat 0px 0px;
+  background-size: contain;
+  background: -o-linear-gradient(transparent, transparent);
+  -webkit-background-clip: text;
+}
+
+.error-block .right-grid {
+  text-align: center;
+  padding: 70px 0px;
+}
+
+.error-block h3 {
+  font-size: 35px;
+  margin-top: 20px;
+  font-weight: 600;
+}
 </style>
